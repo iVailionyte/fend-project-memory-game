10a986816448123a3a42742e6bb9921607dbe182
iVailionyte <indre.vailionyte@gmail.com>
2018-08-15 14:37:02 +0200

    add unlockCards function

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 
 const cards = document.querySelectorAll('.card');
 const deck = document.querySelector('.deck');
-const openCards = [];
+let openCards = [];
 
 /*
  * Display the cards on the page
@@ -36,7 +36,7 @@ const shuffledCards = shuffle([...cards]);
  // - loop through each card and create its HTML
  // - add each card's HTML to the page
 
-//shuffledCards.forEach(card => deck.appendChild(card));
+shuffledCards.forEach(card => deck.appendChild(card));
 
 
 
@@ -45,9 +45,9 @@ const shuffledCards = shuffle([...cards]);
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
-
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ 
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
@@ -61,6 +61,7 @@ cards.forEach(card => {
 	  openCard(card);
     addOpenCard(card);
     lockCards();
+    unlockCards();
 	});
 });
 
@@ -80,7 +81,15 @@ function addOpenCard(card) {
 //  - if the list already has another card, check to see if the two cards match
 function isMatching() {
   if (openCards.length > 1) {
-    return openCards[0].innerHTML === openCards[1].innerHTML;
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
+      openCards.forEach(card => {
+        card.classList.add('match');
+      });
+      removeOpenCards();
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
@@ -94,8 +103,31 @@ function lockCards() {
   }
 }
 
+//  + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
 
+function removeOpenCards() {
+  openCards = [];
+}
 
+function closeCard(card) {
+  card.classList.remove('open');
+  card.classList.remove('show');
+  card.classList.remove('match');
+}
+
+function unlockCards() {
+  console.log(openCards);
+  if (openCards.length > 1) {
+    if (!isMatching()) {
+      setTimeout(() => {
+        openCards.forEach(card => {
+          closeCard(card);
+        });
+        removeOpenCards();
+      }, 1000);
+    }
+  }
+}
 
 
 
