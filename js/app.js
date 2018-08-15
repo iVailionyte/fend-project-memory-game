df4be395ef9d600667379f2cdcc1d6b7e3e88499
iVailionyte <indre.vailionyte@gmail.com>
2018-08-15 17:27:33 +0200

    add moveCounter function

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@
 const cards = document.querySelectorAll('.card');
 const deck = document.querySelector('.deck');
 let openCards = [];
+let moves = 0;
+let counter = document.querySelector(".moves");
 
 /*
  * Display the cards on the page
@@ -47,7 +49,7 @@ shuffledCards.forEach(card => deck.appendChild(card));
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- 
+
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
@@ -62,6 +64,7 @@ cards.forEach(card => {
     addOpenCard(card);
     lockCards();
     unlockCards();
+    moveCounter();
 	});
 });
 
@@ -129,8 +132,11 @@ function unlockCards() {
   }
 }
 
+// + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
 
-
-
+function moveCounter() {
+  moves++;
+  counter.innerHTML = moves;
+}
 
 
